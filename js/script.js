5fcca12f4878f10b774ac55bfa8658419b1466ef
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-03 15:38:39 +0200

    added image from data

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp ({
     data() {
         return {
             message:'Hello World',
+            imageUrl:'https://clipart-library.com/new_gallery/10-108425_earth-png-transparent-background-earth-png.png',
         }
     },
 }).mount('#app');
